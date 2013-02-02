753ef6e0c268cd3d67f835c15caf753b7f24fe78
paulgb <paulgb@gmail.com>
2013-02-02 12:46:01 -0500

    forget

--- data/establishments.R
+++ data/establishments.R
@@ -3,8 +3,10 @@ establishments <- unique(dinesafe[,c('establishment_id','establishment_name','es
 
 establishment_types <- data.frame(table(establishments$establishmenttype))
 establishment_types <- establishment_types[order(-establishment_types$Freq),]
+
+
 establishment_types$rt <- 'Other'
-establishment_types[1:10,'rt'] <- levels(establishment_types$Var1)[establishment_types[1:10,'Var1']]
+establishment_types[1:4,'rt'] <- levels(establishment_types$Var1)[establishment_types[1:4,'Var1']]
 
 establishments <- merge(establishments, establishment_types, by.x='establishmenttype', by.y='Var1')
 
@@ -13,5 +15,8 @@ establishments <- transform(establishments,
     establishmenttype = NULL,
     rt = NULL
 )
+
+levels(establishments$type)[levels(establishments$type) == 'Food Store (Convenience / Variety)'] = 'Food Store'
+levels(establishments$type)[levels(establishments$type) == 'Food Take Out'] = 'Take Out'
 
 write.csv(establishments, 'establishments.csv', row.names=F)
