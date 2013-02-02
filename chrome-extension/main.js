7bb0b2ef3661f2499f6dd6c96ddc2d51cde94d5e
Ted Lee <Ted@TedLee.me>
2013-02-02 02:21:38 -0500

    Switched to fuzzy search api

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -7,18 +7,9 @@ function initalize(){
     var venueID = $("link[rel^='canonical']")[0]["href"].split("/")[5]
     console.log("Current venue is " + venueID);
 
-
-    // Adding a nice little div for us place data
-    $("<div id=\'dine-safe\' class=\'box\'></div>").insertAfter("#venueRating");
-    $("#dine-safe").append("<div class=\'dine-score\'>78</div>");
-    $("#dine-safe").append("<div class=\'dine-title\'>DineSafe Score</div>");
-
-    //$("#dine-safe").append("<table><thead><tr><th>Date</th><th>Pass/Fail?</th></tr></thead><tbody><tr><th>Jan 21 2012</th><th>PASS</th></tr></tbody></table>");
-
-       //<tbody><tr><th>Jan 21 2012</th><th>PASS</th></tr></tbody>
-
-       makeFourSquareRequest(venueID);
-       makeDineSafeRequest();
+    // Add a nice little div for us place data
+    $("<div id=\'dine-safe\' class=\'box\'></div>").insertAfter("#actionBar");
+    makeFourSquareRequest(venueID);
 }
 
 function makeFourSquareRequest(venueID) {
@@ -26,14 +17,19 @@ function makeFourSquareRequest(venueID) {
     $.getJSON(requestURL, function(data){
         lat = data['response']['venue']['location']['lat'];
         lng = data['response']['venue']['location']['lng'];
+        venueName = data['response']['venue']['name']
         console.log("lat is: " + lat + " lng is: " + lng);
+        makeDineSafeRequest(lat, lng, venueName)
     });
 }
 
-function makeDineSafeRequest() {
-    var requestURL = "http://ec2-184-73-102-222.compute-1.amazonaws.com:8080/random"
+function makeDineSafeRequest(lat, lng, venueName) {
+    var requestURL = "http://ec2-184-73-102-222.compute-1.amazonaws.com:8080/fuzzy_match?lat=" + lat + "&lon=" + lng + "&name=" + venueName
     $.getJSON(requestURL, function(data){
 
+        $("#dine-safe").append("<div class=\'dine-score\'>" + data['score'] + "</div>");
+        $("#dine-safe").append("<div class=\'dine-title\'>DineSafe Score</div>");
+        $("#dine-safe").append( data['name'] )
         $("#dine-safe").append("<table><thead><tr><th>Date</th><th>Pass/Fail?</th></tr></thead><tbody>")
 
         for(i in data['inspections']) {
@@ -42,8 +38,8 @@ function makeDineSafeRequest() {
             console.log(date + " : " + result);
             $("#dine-safe").append("<tr><th>" + date + "</th><th>" + result + "</th></tr>")
         }
-
         $("#dine-safe").append("</tbody></table>");
-
     });
 }
+
+
